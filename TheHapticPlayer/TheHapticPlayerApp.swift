f70cd3371f7901ac67833c95b0ea11eb243a9021
ravindusj <ravindualwizz@gmail.com>
2026-04-11 09:19:43 +0530

    Initial Commit

---
+++ TheHapticPlayer/TheHapticPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheHapticPlayerApp.swift
+//  TheHapticPlayer
+//
+//  Created by Ravindu Lachiththa on 2026-04-11.
+//
+
+import SwiftUI
+
+@main
+struct TheHapticPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
